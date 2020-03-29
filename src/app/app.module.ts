2daf90c70a64d52682c376413a326e74f4c44c20
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-03-29 01:11:41 -0300

    tentei colocar um rodapé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeatureRodapeComponent } from './main/feature-rodape/feature-rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FeatureRodapeComponent,
   ],
   imports: [
     BrowserModule,
