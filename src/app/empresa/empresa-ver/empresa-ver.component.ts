6231e84c70242a798ec09b8278f2e6f22b4bf070
emersondantas <emerson.ruan@dce.ufpb.br>
2020-03-28 23:21:15 -0300

    Mudança de estilos em empresa

--- src/app/empresa/empresa-ver/empresa-ver.component.ts
+++ src/app/empresa/empresa-ver/empresa-ver.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EmpresaDTO } from '../models/empresaDTO.entity';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EmpresaMockService } from '../service/empresa.mock.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { UsuarioMockService } from 'src/app/usuarios/service/usuariomock.service';
+import { ValidateBrService } from 'angular-validate-br';
 
 @Component({
   selector: 'app-empresa-ver',
@@ -10,17 +13,45 @@ import { EmpresaMockService } from '../service/empresa.mock.service';
 })
 export class EmpresaVerComponent implements OnInit {
 
+  private formGroup: FormGroup;
   public empresa: EmpresaDTO;
-  
-  constructor(private route: ActivatedRoute, private service: EmpresaMockService) { }
+
+  constructor(private route: ActivatedRoute, private service: EmpresaMockService, private formBuilder: FormBuilder, private router: Router, private usuariosService:UsuarioMockService, private validateBrService: ValidateBrService) { }
 
   ngOnInit() {
-    let id : number = +this.route.snapshot.params["id"];
+    let id: number = +this.route.snapshot.params["id"];
     this.service.getById(id).subscribe(empresa => {
       this.empresa = empresa;
-    }) 
+      this.generateForm();
+    })
   }
-  
+
+  get form() {
+    return this.formGroup.controls;
+  }
+
+  generateForm() {
+    this.formGroup = this.formBuilder.group(
+      {
+        nomeFantasia: [this.empresa.nomeFantasia, []],
+        cnpj: [this.empresa.cnpj, []],
+        razaoSocial: [this.empresa.razaoSocial, []],
+        missao: [this.empresa.missao, []],
+        visao: [this.empresa.visao, []],
+        funcionarios: [this.empresa.funcionarios, []],
+      }
+    );
+    this.deactiveForm();
+  }
+
+  deactiveForm(){
+    this.formGroup.controls["nomeFantasia"].disable();
+    this.formGroup.controls["cnpj"].disable();
+    this.formGroup.controls["razaoSocial"].disable();
+    this.formGroup.controls["missao"].disable();
+    this.formGroup.controls["visao"].disable();
+    this.formGroup.controls["funcionarios"].disable();
+  }
 
 }
 
